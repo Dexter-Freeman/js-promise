62dfb9b7c9dc686f3990bf62c877399413bd5b75
Dexter Freeman <vse.pisma.tut@gmail.com>
2020-04-26 21:25:13 +0300

    Async/await

--- app.js
+++ app.js
@@ -9,25 +9,52 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Функция для получения урлов картинок и для отрисовки их на странице
-    function getAndCreateImgs(queryStr) {
+    // Перепишем эту функцию используя async/await
+    // Объявление async function определяет асинхронную функцию
+    // Цель функций async/await упростить использование promises синхронно
+    // без использования цепочки then()
+    // т.е. мы можем писат асинхронный код, так будто он синхронный (последовательный)
+    async function getAndCreateImgs(queryStr) {
         // делаем запрос на api.giphy.com
         // fetch возвращает объект Promise
-        const promise = fetch(`https://api.giphy.com/v1/gifs/search?api_key=Au06Wg62AqjwnHVBRMMXaz7ReCSTr8Gp&q=${queryStr}&limit=5&offset=0`);
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=Au06Wg62AqjwnHVBRMMXaz7ReCSTr8Gp&q=${queryStr}&limit=5&offset=0`);
         // Преобразуем ответ в объект при помощи метода .json()
         // Т.к. fetch() возвращает промис, который резолвится в объект типа Response, то 
         // для доступа к необходимым нам данным (статусы, код ответа, тело ответа и др)
         // нам необходимо пользоваться свойствами и методами объекта Response
         // например метод .json() распарсит тело (Response.body) как JSON
         // Response.body это объект типа Body у которого доступен в числе прочих метод .json()
-        promise.then(response => response.json())
-            .then(json => {
-                // в данном случае свойство data содержит массив
-                json.data.forEach(img => {
-                    createImg(img.images.downsized_large.url);
-                });
-            })
-            .catch(err => console.error(err));
+        // promise.then(response => response.json())
+        //     .then(json => {
+        //         // в данном случае свойство data содержит массив
+        //         json.data.forEach(img => {
+        //             createImg(img.images.downsized_large.url);
+        //         });
+        //     })
+        //     .catch(err => console.error(err));
+
+        // async/await
+        const json = await response.json();
+        // Оператор await заставляет функцию, объявленную с использованием 
+        // оператора async, ждать выполнения Promise и продолжать выполнение 
+        // после возвращения Promise значения
+        json.data.forEach(img => {
+            createImg(img.images.downsized_large.url);
+        });
+
+        // После вызова функция async возвращает Promise. 
+        // Когда результат был получен, Promise завершается, возвращая полученное значение.  
+        // Когда функция async выбрасывает исключение, Promise ответит отказом 
+        // с выброшенным (throws) значением.
+
+        // Функция async может содержать выражение await, которое приостанавливает 
+        // выполнение функции async и ожидает ответа от переданного Promise, 
+        // затем возобновляя выполнение функции async и возвращая полученное значение.
+
+        // Ключевое слово await допустимо только в асинхронных функциях
     }
+
+
 
     // Выберем форму поиска
     const form = document.forms[0];
@@ -50,52 +77,3 @@ document.addEventListener('DOMContentLoaded', function () {
         // очищаем инпут
         input.value = '';
     });
-
-    // Пример запроса
-    // fetch('https://api.giphy.com/v1/gifs/random?api_key=Au06Wg62AqjwnHVBRMMXaz7ReCSTr8Gp&tag=')
-    //     .then(res => res.json())
-    //     .then(json => {
-    //         createImg(json.data.image_original_url);
-    //     })
-    //     .catch(err => console.error(err));
-
-    // При создании промис находится в ожидании (pending), 
-    // а затем может стать исполненным  (fulfilled), 
-    // вернув полученный результат (значение), 
-    // или отклоненным (rejected), вернув причину отказа. 
-    // В любом из этих случаев вызывается обработчик, прикрепленный к промису методом then.
-
-
-    // Вот пример создания промиса через конструктор
-    // при создании промиса через конструктор необходимо передать ему функцию,
-    // которая будет резолвить промис в случае успеха или реджектить при неудаче
-
-    function delay(time) {
-        return new Promise((resolve, reject) => {
-            // Обработаем reject
-            // в случае неудачи должен быть вызван колбек reject
-            if (isNaN(time)) return reject(new Error('time  must be a number!'));
-
-            // в случае успеха должен быть вызван колбек resolve
-            setTimeout(resolve, time);
-        });
-    }
-
-    // Т.к. конструктор промиса возвращает промис, то мы можем использовать методы then() и catch()
-    function check(time) {
-        delay(time)
-            .then(() => console.log('Promise resolved')) // then() возвращает новый промис и резолвит его
-            // с тем что он возвращает. Т.е. если then() ни чего не вернет, то в следующий промис НИ ЧЕГО
-            // не предастся
-            .then(p => console.log('p', p)) // В консоли будет сообщение: 'p undefined'
-            .catch(err => console.error(err))
-    }
-
-    // Проверим работу. Запустим check  и передадим число 1000
-    // результатом будет сообщение в консоли 'Promise resolved'
-    check(1000); // 'Promise resolved'
-
-    // А теперь передадим строку
-    check('string'); // Error: time  must be a number!
-
-});
